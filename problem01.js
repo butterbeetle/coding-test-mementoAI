53edaa52a16b34d05153e4ee31625d1c7dcb54b6
butterbeetle <butterbeetle@naver.com>
2024-09-30 16:05:50 +0900

    Feat: 각 문제 제한 조건 추가

--- problem01.js
+++ problem01.js
@@ -1,4 +1,12 @@
 const solution = (x, n) => {
+  if (x < -10000000 || x > 10000000) {
+    return "Invalid x value";
+  }
+
+  if (n < 1 || n > 1000) {
+    return "Invalid n value";
+  }
+
   const output = [];
 
   for (let i = 1; i <= n; i++) {
@@ -12,3 +20,10 @@ console.log(solution(2, 5));
 console.log(solution(4, 3));
 console.log(solution(-4, 2));
 console.log(solution(-10, 10));
+console.log(solution(-10000000, 10));
+console.log(solution(-10000001, 10));
+console.log(solution(10000000, 10));
+console.log(solution(10000001, 10));
+console.log(solution(-10, 0));
+console.log(solution(-5, 1));
+console.log(solution(-5, 0));
--- problem02.js
+++ problem02.js
@@ -1,7 +1,14 @@
 const solution = (phone_number) => {
+  if (phone_number.length < 4 || phone_number.length > 20) {
+    return "Invalid phone number value";
+  }
+
   return "*".repeat(phone_number.length - 4) + phone_number.slice(-4);
 };
 
 console.log(solution("01033334444"));
 console.log(solution("027778888"));
 console.log(solution("01086"));
+console.log(solution("0101"));
+console.log(solution("010"));
+console.log(solution("123456789123456789000"));
